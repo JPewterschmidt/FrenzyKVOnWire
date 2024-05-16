93ba5a0e47980af1dcb3ee9753af9fa6a38d54a1
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-05-16 20:56:23 +0800

    forwarding operations to session pimpl

--- server/session.cc
+++ server/session.cc
@@ -1,3 +1,5 @@
+#include <cassert>
+
 #include "toolpex/exceptions.h"
 
 #include "frenzykv/server/session.h"
@@ -7,8 +9,27 @@ namespace frenzykv::server
 
 koios::task<> session_layer::new_connection(toolpex::unique_posix_fd fd)
 {
+    assert(m_pimpl);
     toolpex::not_implemented();
     co_return;
+}
+
+koios::task<> session::update_conn(toolpex::unique_posix_fd fd)
+{
+    assert(m_pimpl);
+    return m_pimpl->update_conn(::std::move(fd));   
+}
+
+const session_id_t& session::session_id() const noexcept
+{
+    assert(m_pimpl);
+    return m_pimpl->session_id();   
+}
+
+const toolpex::uuid session::native_session_id() const
+{
+    assert(m_pimpl);
+    return m_pimpl->native_session_id();   
 }
 
 } // namespace frenzykv::server
